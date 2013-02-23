7d7356c7c9784c024d7fc0c401e535268928b62a
Jerome Touffe-Blin <jtouffeblin@expedia.com>
2013-02-23 17:36:30 +1100

    Move to camera api as getting error with device.capture.

--- js/image.js
+++ js/image.js
@@ -1,19 +1,14 @@
 var Image = {
 	capture: function () {
 		// Launch device camera application,
-		// allowing user to capture up to 3 images
-		navigator.device.capture.captureImage(Image.captureSuccess, Image.captureError, {limit: 3});
+		// allowing user to capture one image
+		navigator.camera.getPicture(Image.captureSuccess, Image.captureError, {quality: 50});
 	},
-	captureSuccess: function (mediaFiles) {
-		var i, len;
-		for (i = 0, len = mediaFiles.length; i < len; i++) {
-	//		uploadFile(mediaFiles[i]);
-			$('#content').append('<p><span>full path:</span><span>' + mediaFiles[i].fullpath +  '</span></p>' +
-					'<p><span>name:</span><span>' + mediaFiles[i].name +  '</span></p><br>');
-		}
+	captureSuccess: function (imageData) {
+		$('#smallImage').attr('src', "data:image/jpeg;base64," + imageData);
+		$('#smallImage').removeClass('hide');
 	},
-	captureError: function (error) {
-		var msg = 'An error occurred during capture: ' + error.code;
+	captureError: function (msg) {
 		navigator.notification.alert(msg, null, 'Uh oh!');
 	}
 };
