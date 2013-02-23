d609766a9533cb739e8d3e20760f62f1cc5c5dd3
Jerome Touffe-Blin <jtouffeblin@expedia.com>
2013-02-23 21:10:33 +1100

    Remove debug, use watchHeading to get update every 100ms.

--- js/compass.js
+++ js/compass.js
@@ -1,15 +1,18 @@
 var Compass = {
 	init: function () {
 		$.mobile.loading('show');
-		navigator.compass.getCurrentHeading(Compass.onSuccess, Compass.onError);
+		navigator.compass.watchHeading(Compass.onSuccess, Compass.onError, Compass.options);
 	},
 	onSuccess: function (heading) {
 		$.mobile.loading('hide');
 		$('#heading').html(heading.magneticHeading);
-		navigator.notification.alert("Compass success, heading: " + heading.magneticHeading, null, 'Uh oh!');
+//		navigator.notification.alert("Compass success, heading: " + heading.magneticHeading, null, 'Uh oh!');
 	},
 	onError: function (compassError) {
 		$.mobile.loading('hide');
 		navigator.notification.alert("Compass error: " + compassError.code, null, 'Uh oh!');
+	},
+	options: {
+		frequency: 100
 	}
 };
