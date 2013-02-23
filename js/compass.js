f9ba5ec24322f172ae0633ba6bcbcc6799a15d21
Jerome Touffe-Blin <jtouffeblin@expedia.com>
2013-02-23 20:08:33 +1100

    Add debug for compass.

--- js/compass.js
+++ js/compass.js
@@ -1,11 +1,15 @@
 var Compass = {
 	init: function () {
+		$.mobile.loading('show');
 		navigator.compass.getCurrentHeading(Compass.onSuccess, Compass.onError);
 	},
 	onSuccess: function (heading) {
+		$.mobile.loading('hide');
 		$('#heading').html(heading);
+		navigator.notification.alert("Compass success.", null, 'Uh oh!');
 	},
-	onError: function (message) {
-		navigator.notification.alert(message, null, 'Uh Oh!');
+	onError: function (code) {
+		$.mobile.loading('hide');
+		navigator.notification.alert("Cannot get heading. Error: " + code, null, 'Uh oh!');
 	}
 };
